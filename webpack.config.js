ace9839f5b1db6237dabe0f9d2a405d1a25e5fe1
norphel <norphel317@gmail.com>
2023-08-23 15:29:53 +0530

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    // eslint-disable-next-line no-undef
+    path: path.resolve(__dirname, "dist"),
+  },
+};
