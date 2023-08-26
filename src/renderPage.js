f723c31753927ee6158034231198e6316f9d7529
norphel <norphel317@gmail.com>
2023-08-26 19:51:00 +0530

    handle incorrect location

--- src/renderPage.js
+++ src/renderPage.js
@@ -1,37 +1,55 @@
 export default function renderPageWithWeatherData(weatherData) {
   weatherData.then((weather) => {
-    displayMainResults(weather);
+    if (weather.current === undefined || weather.location === undefined) {
+      displayMainResults("Error");
+    } else {
+      displayMainResults(weather);
+    }
   });
 }
 
 function displayMainResults(weather) {
-  console.log(weather);
-
   const city = document.querySelector(".city");
-  city.textContent = "";
-  city.textContent = weather.location.name;
-
   const country = document.querySelector(".country");
-  country.textContent = "";
-  country.textContent = weather.location.country;
-
   const dateTime = document.querySelector(".date-time");
-  dateTime.textContent = "";
-  dateTime.textContent = weather.location.localtime;
-
   const temp = document.querySelector(".temp");
-  temp.textContent = "";
-  temp.textContent = weather.current.temp_c;
-
   const weatherIcon = document.querySelector(".icon");
-  weatherIcon.src = "";
-  weatherIcon.src = weather.current.condition.icon;
-
   const condition = document.querySelector(".condition");
-  condition.textContent = "";
-  condition.textContent = weather.current.condition.text;
-
   const feelsLike = document.querySelector(".feels-like");
-  feelsLike.textContent = "";
-  feelsLike.textContent = `Feels like: ${weather.current.feelslike_c}`;
+
+  if (weather === "Error") {
+    city.textContent = "";
+    city.textContent = "Location Not Found!!";
+    country.textContent = "";
+    dateTime.textContent = "";
+    temp.textContent = "";
+    weatherIcon.classList.remove("active");
+    weatherIcon.classList.add("inactive");
+    weatherIcon.src = "";
+    condition.textContent = "";
+    feelsLike.textContent = "";
+  } else {
+    city.textContent = "";
+    city.textContent = weather.location.name;
+
+    country.textContent = "";
+    country.textContent = weather.location.country;
+
+    dateTime.textContent = "";
+    dateTime.textContent = weather.location.localtime;
+
+    temp.textContent = "";
+    temp.textContent = weather.current.temp_c;
+
+    weatherIcon.classList.remove("inactive");
+    weatherIcon.classList.add("active");
+    weatherIcon.src = "";
+    weatherIcon.src = weather.current.condition.icon;
+
+    condition.textContent = "";
+    condition.textContent = weather.current.condition.text;
+
+    feelsLike.textContent = "";
+    feelsLike.textContent = `Feels like: ${weather.current.feelslike_c}`;
+  }
 }
